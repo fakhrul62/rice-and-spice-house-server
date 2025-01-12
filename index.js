5bdb18327f445c9a860e6b7d1a600914ec2fd36f
fakhrul62 <ifakhrul23@gmail.com>
2025-01-12 17:26:25 +0600

    verify admin added

--- index.js
+++ index.js
@@ -36,6 +36,28 @@ async function run() {
     const reviewCollection = client.db("riceDB").collection("reviews");
     const cartCollection = client.db("riceDB").collection("carts");
 
+
+    //jwt api
+    app.post("/jwt", async(req, res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn: "1h"});
+      res.send({token});
+    })
+    //verify token middleware
+    const verifyToken = (req, res, next)=>{
+      console.log("inside verifyToken", req.headers.authorization);
+      if(!req.headers.authorization){
+        return res.status(401).send({message:"Unauthorized Access Brother"});
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded)=>{
+        if(err){
+          return res.status(401).send({message: "Unauthorized Access Brother"});
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
     //users api
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -47,7 +69,21 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
-    app.get("/users", async (req, res) => {
+    app.get("/users/admin/:email", verifyToken, async(req, res)=>{
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({message: "Forbidden Request Brother"});
+      };
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if(user){
+        admin = user?.role === "admin";
+      }
+      res.send({admin});
+
+    })
+    app.get("/users",verifyToken, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
